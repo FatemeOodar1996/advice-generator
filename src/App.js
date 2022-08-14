92458b016feadab008ec3a70fa87b14fcff21301
Fateme Oodar <105479013+FatemeOodar1996@users.noreply.github.com>
2022-08-14 22:58:19 +0430

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Edit and save to reload.</p>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+// import GlobalStyle from "../globalStyles";
+import { useState } from "react";
+import GlobalStyle from "./globalStyles";
+import Advice from "./components/adivce/advice";
+import { useEffect } from "react";
+
+const App = () => {
+    const [advice, setAdvice] = useState();
+
+    useEffect(() => {
+        fetch("https://api.adviceslip.com/advice")
+            .then((response) => response.json())
+            .then((data) => {
+                setAdvice(data.slip);
+            });
+    }, []);
+    return (
+        <div>
+            <GlobalStyle />
+            {advice && <Advice advice={advice} />}
+        </div>
+    );
+};
 
 export default App;
--- src/components/adivce/advice.js
+++ src/components/adivce/advice.js
@@ -1,5 +1,12 @@
 import React from "react";
+import divider from "../../assets/images/pattern-divider-desktop.svg";
 
-function device()=>{
-    
-}
+const Advice = ({ advice }) => (
+    <div>
+        <h1>{advice.id}</h1>
+        <p>{advice.advice}</p>
+        <img src={divider} />
+    </div>
+);
+
+export default Advice;
