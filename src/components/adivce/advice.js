6c02e7ff89bdc6ec6430f4d2446418f15559ec8f
Fateme Oodar <105479013+FatemeOodar1996@users.noreply.github.com>
2022-10-18 21:38:32 +0330

    styles debug-02

--- src/components/adivce/advice.js
+++ src/components/adivce/advice.js
@@ -6,7 +6,7 @@ const Advice = ({ advice }) => (
     <div className="container">
         <div className={`row-cols-4 ${styles.adviceBody}`}>
             <div className={`col ${styles.advice}`}>
-                <h1 className={styles.number}>advice #{advice.id}</h1>
+                <h1 className={styles.number}>ADVICE #{advice.id}</h1>
                 <p className={styles.text}>{advice.advice}</p>
                 <img src={divider} className={styles.image} />
             </div>
